ed91072a70aa71075695d0ebc6a18ffe27bff13f
Jaco Swarts <jaco@productive.me>
2022-08-28 13:36:43 +0200

    Fix merge conflict error

--- src/utils/group-accounts.ts
+++ src/utils/group-accounts.ts
@@ -8,30 +8,11 @@ export interface GroupedAccounts {
 }
 
 export async function groupAccounts(accounts: Account[]) {
-  const grouped: GroupedAccounts = {
-	chequeAccounts: [],
-	creditAccounts: [],
-	savingsAccounts: [],
-	vehicleAccounts: []
-  }
-
-	for(const account of accounts) {
-	const accountType = await account.type()
-	
-	switch (accountType) {
-		case AccountType.Cheque:
-		grouped.chequeAccounts.push(account as AccountCheque)
-		break
-		case AccountType.Credit:
-		grouped.creditAccounts.push(account as AccountCredit)
-		break
-		case AccountType.Savings:
-		case AccountType.Easy:
-		grouped.savingsAccounts.push(account as AccountSavings)
-		break
-		case AccountType.Vehicle:
-		grouped.vehicleAccounts.push(account as AccountVehicle)
-		break
+	const grouped: GroupedAccounts = {
+		chequeAccounts: [],
+		creditAccounts: [],
+		savingsAccounts: [],
+		vehicleAccounts: [],
 	}
 
 	for (const account of accounts) {
@@ -45,6 +26,7 @@ export async function groupAccounts(accounts: Account[]) {
 				grouped.creditAccounts.push(account as AccountCredit)
 				break
 			case AccountType.Savings:
+			case AccountType.Easy:
 				grouped.savingsAccounts.push(account as AccountSavings)
 				break
 			case AccountType.Vehicle:
